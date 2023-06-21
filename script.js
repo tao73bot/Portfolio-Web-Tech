13834ef74832d91658dcb9c515c30b6108fc4815
Md.Taohidul Islam <67187868+tao73bot@users.noreply.github.com>
2023-06-21 18:25:29 +0600

    Day 7 Try to make it responsive

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('.cross').style.display = 'none';
+document.querySelector('.hamburger').addEventListener("click",()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebarGo');
+})
